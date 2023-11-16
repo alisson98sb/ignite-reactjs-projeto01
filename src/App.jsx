b163e1cb4451a62bfd807661e7b6b2dd13307cc4
alisson98sb <alisson98sb@gmail.com>
2023-11-16 16:25:39 -0300

    Iniciando com react - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Post } from "./Post"
 function App() {
   return(
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Alisson Santana Barbosa"
+        content="Enquanto estou indo com o feijão ela está voltando com o rosh montado"
+      />
+
+      <Post 
+        author="Emilly pugin"
+        content="Eu sou um carneirinhoooo"
+      />
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,8 @@
-export function Post() {
-    return <p>Post</p>
+export function Post(propst) {
+    return (
+        <div>
+            <strong>{propst.author}</strong>
+            <h1>{propst.content}</h1>
+        </div>
+    )
 }
